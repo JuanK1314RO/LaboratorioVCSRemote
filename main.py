69aa1d7aa156e4240d0a5c38d68a478c7c401087
Juan Camilo Ramirez Ocampo <juan.ramirez.2020@upb.edu.co>
2020-03-25 13:34:26 -0500

    Mi primer commit

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ Created on Tue Mar 24 14:54:36 2020
 @author: Juan Camilo Ramirez
 """
 
+a = int(input("Ingese el valor de a: "))
+c = int(input("Ingese el valor de b: "))
+
+print("El producto es:", a*c)
+print("El doble de a es", a*2)
