c9c04c032edcd0ecdef85cfca1891a4b6410bfb8
Juan Camilo Ramirez Ocampo <juan.ramirez.2020@upb.edu.co>
2020-03-25 14:26:43 -0500

    Mi prmera modificacion

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ Created on Tue Mar 24 14:54:36 2020
 """
 
 a = int(input("Ingese el valor de a: "))
-c = int(input("Ingese el valor de b: "))
+b = int(input("Ingrese el valor de b: "))
+c = int(input("Ingese el valor de c: "))
+d = int(input("Ingrese el valor de d: "))
 
 print("El producto es:", a*c)
 print("El doble de a es", a*2)
+print("El cuadrado de b es", b**2)
+print("La raiz cuadrada de d es:", d**(1/2))
